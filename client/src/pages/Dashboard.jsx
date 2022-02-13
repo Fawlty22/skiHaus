23a56ee7a5ee79b843fb4fb881a02d84e3eb668b
Chad Branagan <chad.branagan@gmail.com>
2022-02-12 17:59:45 -0800

    merge

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,43 +1,15 @@
-<<<<<<< HEAD
-import React from "react";
-import { Redirect } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { Card, Button } from "react-bootstrap";
-// import { QUERY_BOOKS } from '../graphql/queries';
-=======
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 import { QUERY_EMPLOYEE } from '../graphql/queries';
 import { StoreProvider } from '../utils/GlobalContext';
->>>>>>> 59cb32bd0c4ce8f3d9ffc9ff134183f05644f898
 
 const Dashboard = () => {
   
   let employee = Auth.getProfile();
   console.log(employee)
 
-<<<<<<< HEAD
-  //redirect to login if error in a query, not defined yet
-  //   if (error) {
-  //     return <Redirect to={"/login"} />
-  //   }
-
-  return (
-    <Card>
-      <Card.Header as="h5">Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-};
-=======
   // redirect to login if error in a query, not defined yet
   if (!employee) {
     return <Redirect to={"/login"} />
@@ -57,6 +29,5 @@ const Dashboard = () => {
     </div>
     );
   };
->>>>>>> 59cb32bd0c4ce8f3d9ffc9ff134183f05644f898
 
 export default Dashboard;
