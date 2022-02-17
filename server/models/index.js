948c56003c8c939e968b5f31edc28f1cc8a617a2
Chad Branagan <chad.branagan@gmail.com>
2022-02-16 16:40:35 -0800

    commit to merge

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,5 @@
-const Customer = require('./Customer');
-const Equipment = require('./Equipment');
 const Contract = require('./Contract');
 const Employee = require('./Employee');
 const User = require('./User');
 
-module.exports = { Employee, Customer, Equipment, Contract, User  };
+module.exports = { Employee, Contract, User  };
