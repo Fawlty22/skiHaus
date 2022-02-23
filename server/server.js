26d6b441b021322c2b5cb51a8e9c9077dc5f046c
Matt Keys <MatthewjKeys22@gmail.com>
2022-02-22 22:28:21 -0700

    please work

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 // import middleware for verifying token sent with server requests
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -28,6 +29,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // log where we can go to test our GQL API
 console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
