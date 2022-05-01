756ed0a9abc8cf666fbe5d0e426abfc2573edc4b
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-01 14:19:40 -0600

    heroku build code in server.js was commented out for some reason, redoing the heroku build

--- server/server.js
+++ server/server.js
@@ -32,12 +32,12 @@ app.use(express.json());
 
 
 // // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // log where we can go to test our GQL API
 console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
